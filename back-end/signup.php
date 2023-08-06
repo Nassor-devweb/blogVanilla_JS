97a95408956b48103bae711f09a78a1f5f3913e6
HIMIDI Ahmed <nassor.devweb@gmail.com>
2023-08-06 21:00:27 +0200

    implement login back

--- back-end/signup.php
+++ back-end/signup.php
@@ -1,15 +1,28 @@
-<?
+<?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Credentials: true");
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-}
+$errors = [
+    'name_user' => "Le nom d'utilisateur est incorrect 2 lettre minimale et 8 maximale",
+    'password_user' => 'Le mots de passe est incorrect 5 lettre minimale et 14 maximale',
+    'email_user' => "L'adresse email est incorrect"
+];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    header('Content-Type: application/json');
-    http_response_code(200);
-    echo json_encode($_POST);
+    $user_password = $_POST['user_password'];
+    $user_name = $_POST['user_name'];
+    $user_email = $_POST['user_email'];
+
+    $error = match (true) {
+        mb_strlen(trim($user_password)) < 5 || mb_strlen(trim($user_password)) > 14 => $errors['password_user'],
+        mb_strlen(trim($user_name)) < 2 || mb_strlen(trim($user_name)) > 8 => $errors['name_user'],
+        filter_var($user_email, FILTER_VALIDATE_EMAIL) === false => $errors['email_user'],
+        default => ''
+    };
+
+    if (!$error) {
+    } else {
+        http_response_code(400);
+        $err = [];
+        $err['erreur'] = $error;
+        echo json_encode($err);
+    }
 }
--- front-end/src/signup/signup.js
+++ front-end/src/signup/signup.js
@@ -7,7 +7,8 @@ form.addEventListener('submit', async (e) => {
     const user_name = document.querySelector('#user_name').value
     const user_email = document.querySelector('#user_email').value
     const user_password = document.querySelector('#user_password').value
-    const user_photo = document.querySelector('#user_photo').value
+    const user_photo = document.querySelector('#user_photo').files[0]
+    console.dir(user_photo)
     const formdata = new FormData()
     formdata.append('user_name', user_name)
     formdata.append('user_email', user_email)
@@ -16,9 +17,15 @@ form.addEventListener('submit', async (e) => {
     try {
         const querrySaveUser = await fetch('http://localhost:3000/signup.php', {
             method: 'POST',
-
             body: formdata
         })
+        if(querrySaveUser.ok){
+            const response = await querrySaveUser.json()
+            console.log(response)
+        }else{
+            const response = await querrySaveUser.json()
+            console.log(response)
+        }
         console.log(querrySaveUser)
     } catch (err) {
         console.log(err)
